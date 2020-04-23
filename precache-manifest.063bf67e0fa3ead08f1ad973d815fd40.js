f051aec9a06051b2a7bf270e6c16f897779f3a23
Thijs Smudde <thijssmudde@kateinnovations.com>
2020-04-23 12:57:03 +0200

    Updates

---
+++ precache-manifest.063bf67e0fa3ead08f1ad973d815fd40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0040a199122df8b01238d40819a0d25",
+    "url": "/case1.github.io/index.html"
+  },
+  {
+    "revision": "0d2803c6cbd90b984d83",
+    "url": "/case1.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "719d7adbe8f72f1d4722",
+    "url": "/case1.github.io/static/js/2.73f6acdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/case1.github.io/static/js/2.73f6acdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2803c6cbd90b984d83",
+    "url": "/case1.github.io/static/js/main.1c98b67e.chunk.js"
+  },
+  {
+    "revision": "3b368b117e81070a32a8",
+    "url": "/case1.github.io/static/js/runtime-main.c2e42f1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/case1.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
